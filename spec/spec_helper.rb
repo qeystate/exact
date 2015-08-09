22740c2e298ddc2fce1020b7e1f1514fbd336c70
zzk <zzk@chickenkiller.org>
2015-08-09 14:11:48 +0200

    Cleanup spec_helpers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,17 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'exact'
+require 'vcr'
+require 'factory_girl'
+
+FactoryGirl.find_definitions
+
+VCR.configure do |c|
+  c.hook_into :faraday
+  c.cassette_library_dir = 'spec/vcr'
+  c.default_cassette_options = { record: :new_episodes }
+end
+
+RSpec.configure do |config|
+  config.before(:suite) { Exact.configure }
+  config.include FactoryGirl::Syntax::Methods
+end
