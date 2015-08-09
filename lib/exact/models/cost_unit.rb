81106cee2fdc68e466988399605d11c8f7a2bd76
zzk <zzk@chickenkiller.org>
2015-08-09 21:54:31 +0200

    Fix endpoint for CostUnit

--- lib/exact/models/cost_unit.rb
+++ lib/exact/models/cost_unit.rb
@@ -3,8 +3,9 @@ module Exact
     include ActiveModel::Model
     include Virtus.model
 
-    EXACT_SERVICE = 'hrm'
-    EXACT_GUID    = 'ID'
+    EXACT_SERVICE  = 'hrm'
+    EXACT_GUID     = 'ID'
+    EXACT_ENDPOINT = 'Costunits'
 
     attribute :Code,             String
     attribute :Created,          DateTime
