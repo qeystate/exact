68fa4d5712f4ae4ba31e2163330595c5a1e86e57
zzk <zzk@chickenkiller.org>
2015-08-27 15:47:44 +0200

    Introduce layout

---
+++ lib/exact/models/layout.rb
@@ -0,0 +1,20 @@
+module Exact
+  class Layout < Base
+    include ActiveModel::Model
+    include Virtus.model
+
+    EXACT_SERVICE = 'salesinvoice'
+    EXACT_GUID    = 'InvoiceID'
+
+    attribute :ID,               String
+    attribute :Created,          DateTime
+    attribute :Creator,          String
+    attribute :CreatorFullName,  String
+    attribute :Division,         String
+    attribute :Modified,         DateTime
+    attribute :Modifier,         String
+    attribute :ModifierFullName, String
+    attribute :Subject,          String
+    attribute :Type,             Integer
+  end
+end
