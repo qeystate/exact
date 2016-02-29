8e50393b988a267de717529553b0dd9f05fd1448
Ruben Homs <rubenhoms@gmail.com>
2016-02-29 10:03:07 +0100

    Added division mapping to get all customers

---
+++ lib/exact/mappings/division_mapping.rb
@@ -0,0 +1,4 @@
+module Exact
+  class DivisionMapping < BaseMapping
+  end
+end
---
+++ lib/exact/models/division.rb
@@ -0,0 +1,33 @@
+module Exact
+  class Division < Base
+    include ActiveModel::Model
+    include Virtus.model
+
+    EXACT_SERVICE = 'hrm'
+    EXACT_GUID    = 'Code'
+
+    attribute :Code,                Integer
+    attribute :BlockingStatus,      Integer
+    attribute :Country,             String
+    attribute :CountryDescription,  String
+    attribute :Created,             DateTime
+    attribute :Creator,             String
+    attribute :CreatorFullName,     String
+    attribute :Currency,            String
+    attribute :CurrencyDescription, String
+    attribute :Customer,            String
+    attribute :Description,         String
+    attribute :HID,                 Integer
+    attribute :Main,                Boolean
+    attribute :Modified,            DateTime
+    attribute :Modifier,            String
+    attribute :ModifierFullName,    String
+    attribute :SiretNumber,         String
+    attribute :StartDate,           DateTime
+    attribute :Status,              Integer
+    attribute :TaxOfficeNumber,     String
+    attribute :TaxReferenceNumber,  String
+    attribute :VATNumber,           String
+    attribute :Website,             String
+  end
+end
