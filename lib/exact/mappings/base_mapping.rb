b4a88c003138a8d023637e950cbb323cf09404af
Ruben Homs <rubenhoms@gmail.com>
2016-12-12 10:28:54 +0100

    Only map attributes we know of, don't assume Exact API

--- lib/exact/mappings/base_mapping.rb
+++ lib/exact/mappings/base_mapping.rb
@@ -1,15 +1,16 @@
 module Exact
   class BaseMapping
     def self.convert(exact_object, object = nil)
-      hsh = exact_object.instance_variables.each_with_object({}) { |var, hash| hash[var.to_s.delete('@').to_sym] = exact_object.instance_variable_get(var) }.except!(:__metadata)
+      name = to_s.demodulize
+      name.slice!('Mapping')
+      attributes = Exact.const_get(name).attributes.to_a.map{|a| a.options[:name].to_s }
+      hsh = exact_object.instance_variables.each_with_object({}) { |var, hash| hash[var.to_s.delete('@').to_sym] = exact_object.instance_variable_get(var) if attributes.include? var.to_s.delete('@') }.except!(:__metadata)
       if object.present?
         hsh.each do |k, v|
           object.send("#{k}=", v)
         end
         object
       else
-        name = to_s.demodulize
-        name.slice!('Mapping')
         Exact.const_get(name).new(hsh) unless object.present?
       end
     end
--- lib/exact/version.rb
+++ lib/exact/version.rb
@@ -1,3 +1,3 @@
 module Exact
-  VERSION = '0.2.6'
+  VERSION = '0.2.7'
 end
